ab9722eb481383acac6e7c6cb315531fc9422c79
Ariful Islam Siyam <siyam34343434@gmail.com>
2026-08-02 22:28:09 +0600

    Update admin.h

--- include/admin.h
+++ include/admin.h
@@ -1,1 +1,12 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+#include "types.h"
 
+int admin_dashboard();
+int admin_list_accounts();
+int admin_view_account();
+int admin_reset_pin();
+int admin_toggle_account();
+int admin_view_all_loans();
+
+#endif
