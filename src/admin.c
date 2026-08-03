84f1662c469a05203f60cf6b9f4c59ddd91f187d
Ariful Islam Siyam <siyam34343434@gmail.com>
2026-08-03 11:38:14 +0600

    Update admin.c
    ongoing

--- src/admin.c
+++ src/admin.c
@@ -33,3 +33,34 @@ int admin_list_accounts() { //syntax fixed
     press_enter(); //added this
     return 0;
 }
+
+int admin_view_account() {
+    int id;
+    Account acc;
+    Transaction txns[MAX_TRANSACTIONS];
+    Loan loans[MAX_LOANS];
+    int txn_count = 0, loan_count = 0, i, found;
+
+    print_header("VIEW ACCOUNT DETAILS");
+    id = get_int("  Enter account ID: ", STARTING_ACCOUNT_ID, 99999);
+    if (!find_account(id, &acc)) { print_error("Account not found."); press_enter(); return 1; }
+    print_account_card(&acc);
+
+    load_transactions(txns, &txn_count);
+    printf("\n  Recent Transactions:\n");
+    print_txn_table_header();
+    found = 0;
+    for (i = txn_count - 1; i >= 0 && found < 10; i--)
+        if (txns[i].account_id == id) { print_transaction_row(&txns[i]); found++; }
+    if (found == 0) printf(DIM "  No transactions.\n" RESET);
+
+    load_loans(loans, &loan_count);
+    printf("\n  Loans:\n");
+    print_loan_table_header();
+    found = 0;
+    for (i = 0; i < loan_count; i++)
+        if (loans[i].account_id == id) { print_loan_row(&loans[i]); found++; }
+    if (found == 0) printf(DIM "  No loans.\n" RESET);
+    press_enter();
+    return 0;
+}
