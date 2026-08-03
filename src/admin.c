24be5a245b026164dec47421e1203b8057cbcf07
Ariful Islam Siyam <siyam34343434@gmail.com>
2026-08-03 11:30:55 +0600

    Update admin.c
    ongoing

--- src/admin.c
+++ src/admin.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "../include/types.h"
+#include "../include/admin.h"
+#include "../include/account.h"
+#include "../include/utils.h"
+#include "../include/file_io.h"
+#include "../include/display.h"
 
+int admin_dashboard() {
+    Account accounts[MAX_ACCOUNTS];
+    Loan loans[MAX_LOANS];
+    int acc_count = 0, loan_count = 0, active = 0, active_loans = 0, i;
+    double total_balance = 0.0, loan_balance = 0.0;
+    load_accounts(accounts, &acc_count);
+    load_loans(loans, &loan_count);
+    for (i = 0; i < acc_count; i++) if (accounts[i].is_active) { active++; total_balance += accounts[i].balance; }
+    for (i = 0; i < loan_count; i++) if (loans[i].status == LOAN_ACTIVE) { active_loans++; loan_balance += loans[i].remaining_balance; }
+    print_admin_stats(acc_count, active, total_balance, active_loans, loan_balance);
+    return 0;
+}
