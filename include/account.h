81f6ed1c5a4fd79cc948b0f5128cbb3aca3c15c9
dlsjitu-cyber <dlsjitu@gmail.com>
2026-08-07 00:00:55 +0600

    Update account.h

--- include/account.h
+++ include/account.h
@@ -1,1 +1,23 @@
+#ifndef ACCOUNT_H
+#define ACCOUNT_H
+
+
+struct account
+{
+    int id;
+    char name[30];
+    char type[20];
+    char pin[20];
+    char userType[20];
+    float balance;
+    int active;
+};
+
+int create_account();
+int find_account();
+int view_account();
+int update_balance(int searchID, float new_balance);
+int close_account();
+
+#endif
 
