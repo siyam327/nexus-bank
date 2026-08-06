b1f2ddb81708ac07f70623bda0f226c638aa55eb
dlsjitu-cyber <dlsjitu@gmail.com>
2026-08-07 00:05:11 +0600

    Update account.c

--- src/account.c
+++ src/account.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include "account.h"
 
+int create_account()
+{
+    struct account acc;
+    FILE *fp;
+    int maxID=1000;
+    int choice;
+    int option;
+    char confirm_pin[20];
+    int i;
+
+
+    fp= fopen("account.dat", "rb");
+
+    if(fp==NULL)
+    {
+        acc.id=1001;
+    }
+    else
+    {
+        while(fread(&acc,sizeof(acc),1,fp))
+       {
+           if(acc.id>maxID)
+           {
+               maxID=acc.id;
+           }
+       }
