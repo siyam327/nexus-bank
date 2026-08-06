a92901a12247e5eb6701c57dea0f5a5f056d3078
dlsjitu-cyber <dlsjitu@gmail.com>
2026-08-07 00:08:25 +0600

    Update account.c

--- src/account.c
+++ src/account.c
@@ -28,3 +28,33 @@ int create_account()
                maxID=acc.id;
            }
        }
+acc.id=maxID+1;
+        fclose(fp);
+    }
+
+
+    printf("Enter account holders name:");
+    getchar();
+    fgets(acc.name,30,stdin);
+
+    if(strlen(acc.name)<2){
+        printf("Invalid name");
+        return 0;
+    }
+
+    printf("Account type:\n");
+    printf("1.Savings\n");
+    printf("2.Checking\n");
+    printf("Enter a number:");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+        strcpy(acc.type,"Savings");
+    }
+    else if(choice==2)
+    {
+        strcpy(acc.type,"Checking");
+    }
+    else
+    {
+        printf("Invalid choice");
